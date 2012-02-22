3268d37525647bd0d86ba7459718663697123e01
Mak Nazečić-Andrlon <owlberteinstein@gmail.com>
2012-02-22 14:03:57 +0100

    Add featured treasury model.

---
+++ lib/etsy/featured_treasury.rb
@@ -0,0 +1,35 @@
+module Etsy
+	class FeaturedTreasury
+
+		include Etsy::Model
+
+		attribute :id, :from => :treasury_id
+		attribute :owner_id, :from => :treasury_owner_id
+		attributes :url, :region, :active_date
+
+
+		def self.find_all(options={})
+			get_all('/featured_treasuries', options)
+		end
+
+		def self.find(*identifiers_and_options)
+			find_one_or_more('featured_treasuries', identifiers_and_options)
+		end
+
+		def self.find_all_by_owner(user_id, options = {})
+			get_all("/featured_treasuries/owner/#{user_id}", options)
+		end
+
+		def owner(*options)
+			Etsy::User.find(owner_id, *options)
+		end
+
+		def treasury(*options)
+			Etsy::Treasury.find(URI(url).path.split('/').last, *options)
+		end
+
+		def activated_at
+			Time.at(active_date)
+		end
+	end
+end
